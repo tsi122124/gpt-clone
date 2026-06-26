b3513cffa10b3661a48931adb1d4197a5e8fdc32
tsi122124 <tsionhabte6@gmail.com>
2026-06-26 17:49:39 +0300

    Add database port for deployment

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -1,7 +1,8 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
