5494c0e90d7da9b3ef886910e2905296ac42e9a5
tsi122124 <tsionhabte6@gmail.com>
2026-06-29 11:02:28 +0300

    Fix CORS for deployment

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,7 @@ import cors from "cors";
 import { errorHandler } from "./src/middleware/error-handler.js";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", mainRouter);
